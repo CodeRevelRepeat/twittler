b28b58f739d2e5632a4c41f50740555d3d5daf7f
Lauren <lhspiegel@gmail.com>
2015-02-16 11:20:48 -0800

    Adds moment.js to add human friendly timestamps

--- javascript2.js
+++ javascript2.js
@@ -6,26 +6,40 @@
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + " tweeted at " + tweet.created_at);
+          $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
+            " tweeted at " + moment(tweet.created_at).fromNow());
           $tweet.appendTo($tweetbody);
           index -= 1;
           shownTweets +=1;
         }
 
 
-        //This is the option to show new tweets through button.  They are not appearing in the correct order.  
+        //This is the option to show new tweets through button.  They are not appearing in the correct order. 
+         
 
         $('button').on('click', function(){
-
-        while((streams.home.length - 1) > shownTweets){
-          var tweet = streams.home[streams.home.length - (1 + shownTweets)];
+          var totalTweetsNow = streams.home.length - 1;
+        while(totalTweetsNow > shownTweets){
+          var tweet = streams.home[totalTweetsNow - shownTweets];
           var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + " tweeted at " + tweet.created_at);
-          $tweet.prependTo($tweetbody);
+          var userName = tweet.user
+          $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
+            " tweeted at " + moment(tweet.created_at).fromNow());
+          
+          //Added fadeIn here.  Can remove if looks like tweets are loading slowly rather than nice effect.
+
+          $tweet.fadeIn("slow").prependTo($tweetbody);
           shownTweets +=1;
+
         }
 
         });
+
+
+
+
+
+        
 
 
         //This is the option to show new tweets automatically.  They are not appearing in the correct order.  
@@ -44,6 +58,17 @@
         // setInterval(checkNew, 6000);
 
 
+
+
+        //Click on username to see that user's timeline:
+
+        $('.user').on('click', function(){
+            var userName = $(this).data('user');
+            console.log(userName);
+            console.log(streams.users.shawndrost);
+      
+
+        });
 
 
 
