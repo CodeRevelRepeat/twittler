3b09b76338449ebd0b95ff2506e3ea3be193077e
Lauren <lhspiegel@gmail.com>
2015-02-16 11:31:09 -0800

    Fixes order in which new tweets appear by refreshing all tweets on button click rather than just appending new tweets  Please enter the commit message for your changes. Lines starting

--- javascript2.js
+++ javascript2.js
@@ -1,4 +1,6 @@
      $(document).ready(function(){
+
+
         var $tweetbody = $('.tweets');
         $tweetbody.html('');
         var index = streams.home.length - 1;
@@ -7,42 +9,36 @@
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
           $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
-            " tweeted at " + moment(tweet.created_at).fromNow());
+            " tweeted " + moment(tweet.created_at).fromNow());
           $tweet.appendTo($tweetbody);
           index -= 1;
           shownTweets +=1;
         }
 
 
-        //This is the option to show new tweets through button.  They are not appearing in the correct order. 
-         
+
 
         $('button').on('click', function(){
-          var totalTweetsNow = streams.home.length - 1;
-        while(totalTweetsNow > shownTweets){
-          var tweet = streams.home[totalTweetsNow - shownTweets];
-          var $tweet = $('<div></div>');
-          var userName = tweet.user
-          $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
-            " tweeted at " + moment(tweet.created_at).fromNow());
-          
-          //Added fadeIn here.  Can remove if looks like tweets are loading slowly rather than nice effect.
-
-          $tweet.fadeIn("slow").prependTo($tweetbody);
-          shownTweets +=1;
-
-        }
-
-        });
+          var $tweetbody = $('.tweets');
+          $tweetbody.html('');
+          var index = streams.home.length - 1;
+          //var shownTweets = 0;
+          while(index >= 0){
+            var tweet = streams.home[index];
+            var $tweet = $('<div></div>');
+             $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
+              " tweeted " + moment(tweet.created_at).fromNow());
+            $tweet.fadeIn("slow").appendTo($tweetbody);
+            index -= 1;
+           
+            }
+      });
 
 
-
-
-
-        
 
 
         //This is the option to show new tweets automatically.  They are not appearing in the correct order.  
+        //Problem is not updating full list of tweets wich should do especially to update time stamps in human friendly form. 
 
         // var checkNew = function(){
         //   while((streams.home.length - 1) > shownTweets){
