152eac9ffcd2d64f70c879cbfbb0ae2f2efc2dca
Lauren <lhspiegel@gmail.com>
2015-02-16 14:10:48 -0800

    Adds styling

--- javascript2.js
+++ javascript2.js
@@ -4,15 +4,16 @@
         var $tweetbody = $('.tweets');
         $tweetbody.html('');
         var index = streams.home.length - 1;
-        var shownTweets = 0;
+
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
-            " tweeted " + moment(tweet.created_at).fromNow());
+          $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': <br>' 
+              + '<span class = "message">' + tweet.message + '</span><br>' + 
+             " tweetled " + '<span class = "time">' + moment(tweet.created_at).fromNow() + '</span><br><br>');
           $tweet.appendTo($tweetbody);
           index -= 1;
-          shownTweets +=1;
+       
         }
 
 
@@ -22,12 +23,12 @@
           var $tweetbody = $('.tweets');
           $tweetbody.html('');
           var index = streams.home.length - 1;
-          //var shownTweets = 0;
           while(index >= 0){
             var tweet = streams.home[index];
             var $tweet = $('<div></div>');
-             $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': ' + tweet.message + 
-              " tweeted " + moment(tweet.created_at).fromNow());
+            $tweet.html('@' + '<span class = "user">' + tweet.user + '</span>' + ': <br>' 
+              + '<span class = "message">' + tweet.message + '</span><br>' + 
+             " tweetled " + '<span class = "time">' + moment(tweet.created_at).fromNow() + '</span><br><br>');
             $tweet.fadeIn("slow").appendTo($tweetbody);
             index -= 1;
            
@@ -37,7 +38,8 @@
 
 
 
-        //This is the option to show new tweets automatically.  They are not appearing in the correct order.  
+        //This is the option to show new tweets automatically.  Relied on a shownTweets variable deleted above.
+        //In any case, tweets were not appearing in the correct order.  
         //Problem is not updating full list of tweets wich should do especially to update time stamps in human friendly form. 
 
         // var checkNew = function(){
@@ -58,13 +60,25 @@
 
         //Click on username to see that user's timeline:
 
-        $('.user').on('click', function(){
+        $('.user').on('click', function(event){
+            event.preventDefault();
+            event.stopPropagation();
             var userName = $(this).data('user');
             console.log(userName);
             console.log(streams.users.shawndrost);
       
 
         });
+
+
+        //Writing a tweet
+
+        var visitor = "visitor";
+        streams.users.visitor = [];
+        $('.submitTweet').on('click', function(){
+            var tweetMessage = $('.writeTweet').val();
+            writeTweet(tweetMessage);
+        })
 
 
 
